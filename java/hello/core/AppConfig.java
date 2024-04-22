3d3646154a7ea43fb7ef6f89445966f7ea7d0e9b
jinseongHwang <wlstjd2116@gmail.com>
2024-04-22 21:44:08 +0900

    20240422 commit

--- java/hello/core/AppConfig.java
+++ java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -9,22 +8,26 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderSerivce;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-public class Appconfig {
+@Configuration
+public class AppConfig {
 
     // 생성자 주입
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(MemberRepository());
     }
-
-    private static MemberRepository MemberRepository() {
+    @Bean
+    public static MemberRepository MemberRepository() {
         return new MemoryMemberRepository();
     }
-
-    public OrderSerivce orderSerivce() {
+    @Bean
+    public OrderSerivce orderService() {
         return new OrderServiceImpl(MemberRepository(), discountPolicy());
     }
-
+    @Bean
     // 할인 정책이 변경되었을 때, 아래의 클래스만 변경해주면 된다.
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
--- java/hello/core/order/OrderApp.java
+++ java/hello/core/order/OrderApp.java
@@ -1,27 +1,34 @@
 package hello.core.order;
 
-import hello.core.Appconfig;
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 
-        Appconfig appconfig = new Appconfig();
-        MemberService memberService = appconfig.memberService();
-        OrderSerivce orderSerivce = appconfig.orderSerivce();
+//        AppConfig appconfig = new AppConfig();
+//        MemberService memberService = appconfig.memberService();
+//        OrderSerivce orderSerivce = appconfig.orderSerivce();
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderSerivce orderService = ac.getBean("orderService", OrderSerivce.class);
+
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderSerivce.createOrder(memberId, "itemA", 20000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
 
-        System.out.println("order = " +order);
-        System.out.println("order.calculatePrice = " +order.calculatePrice());
+        System.out.println("order = " + order);
+        System.out.println("order.calculatePrice = " + order.calculatePrice());
 
     }
 }
